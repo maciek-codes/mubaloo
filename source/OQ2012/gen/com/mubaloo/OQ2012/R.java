112bdad07ee12a44e25c94f97bbbb7d1366f5fec
Ricardo Rendon Cepeda <rr1461@my.bristol.ac.uk>
2012-03-10 13:20:19 +0000

    Interface started

--- source/OQ2012/gen/com/mubaloo/OQ2012/R.java
+++ source/OQ2012/gen/com/mubaloo/OQ2012/R.java
@@ -12,19 +12,32 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int mbronze=0x7f020002;
-        public static final int mgold=0x7f020003;
-        public static final int msilver=0x7f020004;
-        public static final int mytextarea=0x7f020005;
-        public static final int torch1=0x7f020006;
-        public static final int torch2=0x7f020007;
-        public static final int torch3=0x7f020008;
-        public static final int torch4=0x7f020009;
-        public static final int torch5=0x7f02000a;
+        public static final int button=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int mbronze=0x7f020003;
+        public static final int mgold=0x7f020004;
+        public static final int msilver=0x7f020005;
+        public static final int mytextarea=0x7f020006;
+        public static final int torch=0x7f020007;
+        public static final int torch1=0x7f020008;
+        public static final int torch2=0x7f020009;
+        public static final int torch3=0x7f02000a;
+        public static final int torch4=0x7f02000b;
+        public static final int torch5=0x7f02000c;
+    }
+    public static final class id {
+        public static final int b_countdown=0x7f050006;
+        public static final int b_follow=0x7f050003;
+        public static final int b_info=0x7f050004;
+        public static final int b_play=0x7f050001;
+        public static final int b_stats=0x7f050002;
+        public static final int iv_torch=0x7f050007;
+        public static final int linearLayout1=0x7f050000;
+        public static final int linearLayout2=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int oq2012activity=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- source/OQ2012/src/com/mubaloo/OQ2012/OQ2012Activity.java
+++ source/OQ2012/src/com/mubaloo/OQ2012/OQ2012Activity.java
@@ -1,16 +1,109 @@
 package com.mubaloo.OQ2012;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class OQ2012Activity extends Activity {
-    /** Called when the activity is first created. */
+public class OQ2012Activity extends Activity 
+{
+	// OOP elements
+	private static OQ2012Activity instance;
+	
+	// UI elements
+	private Button b_play;
+	private Button b_stats;
+	private Button b_follow;
+	private Button b_info;
+	private Button b_countdown;
+    private Typeface customFont;
+    //private ImageView iv_torch;
+    //private AnimationDrawable ad_torch;
+	
+    // Called when the activity is first created.
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Olympic Quiz 2012");
-        setContentView(tv);
+        setContentView(R.layout.oq2012activity);
+        instance = this;
+        
+        customFont 	= Typeface.createFromAsset(getAssets(), "fonts/LONDON2012.TTF");
+        
+        b_play = (Button) findViewById(R.id.b_play);
+        b_play.setText("PLAY!");
+        b_play.setTextColor(Color.WHITE);
+        b_play.setTypeface(customFont);
+        
+        b_stats = (Button) findViewById(R.id.b_stats);
+        b_stats.setText("STATS");
+        b_stats.setTextColor(Color.WHITE);
+        b_stats.setTypeface(customFont);
+        
+        b_follow = (Button) findViewById(R.id.b_follow);
+        b_follow.setText("FOLLOW");
+        b_follow.setTextColor(Color.WHITE);
+        b_follow.setTypeface(customFont);
+        
+        b_info = (Button) findViewById(R.id.b_info);
+        b_info.setText("INFO");
+        b_info.setTextColor(Color.WHITE);
+        b_info.setTypeface(customFont);
+        
+        b_countdown = (Button) findViewById(R.id.b_countdown);
+        b_countdown.setText("DAYS TO GO: XXX");
+        b_countdown.setTextColor(Color.WHITE);
+        b_countdown.setTypeface(customFont);
+        
+        /*
+        iv_torch = (ImageView) findViewById(R.id.iv_torch);
+        iv_torch.setBackgroundResource(R.drawable.torch);
+        ad_torch = (AnimationDrawable) iv_torch.getBackground();
+        ad_torch.start();
+        */
+        
+        /*ad_torch = new AnimationDrawable();
+        ad_torch.addFrame(getResources().getDrawable(R.drawable.torch1),200);
+        ad_torch.addFrame(getResources().getDrawable(R.drawable.torch2),200);
+        ad_torch.addFrame(getResources().getDrawable(R.drawable.torch3),200);
+        ad_torch.addFrame(getResources().getDrawable(R.drawable.torch4),200);
+        ad_torch.addFrame(getResources().getDrawable(R.drawable.torch5),200);
+        ad_torch.setOneShot(false);
+        
+        iv_torch = (ImageView) findViewById(R.id.iv_torch);
+        iv_torch.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        iv_torch.setImageDrawable(ad_torch);
+        ad_torch.start();*/
+        
+        // Button actions
+        View.OnClickListener handler = new View.OnClickListener() 
+        {
+			@Override
+			public void onClick(View v) 
+			{
+				switch(v.getId())
+				{
+					case R.id.b_play:
+						//GO TO SCREEN
+						//Intent info_Intent = new Intent(v.getContext(), Info.class);
+						//startActivityForResult(info_Intent, 0);
+					break;
+					
+					case R.id.b_stats:
+						//GO TO SCREEN
+					break;
+					
+					case R.id.b_follow:
+						//GO TO SCREEN
+					break;
+					
+					case R.id.b_info:
+						//GO TO SCREEN
+					break;
+				}	
+			}
+		};
     }
 }
