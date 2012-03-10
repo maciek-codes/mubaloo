c963a40704ec3b6ca9bc9a395fe6832b824f358a
Madalina Patrichi <madalinapatrichi@yahoo.com>
2012-03-10 16:09:03 +0000

    listview

--- source/OQ2012/src/com/mubaloo/OQ2012/Question.java
+++ source/OQ2012/src/com/mubaloo/OQ2012/Question.java
@@ -5,21 +5,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.http.util.ByteArrayBuffer;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class Question extends Activity implements OnClickListener {
+public class Question extends ListActivity implements OnClickListener {
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +32,10 @@ public class Question extends Activity implements OnClickListener {
         String getName = null;
         TextView countryName;
         TextView countryQuestion;
-        ListView answer;
+        ListView answer_list;
         countryName = (TextView)findViewById(R.id.countryName);
         countryQuestion = (TextView)findViewById(R.id.countryQuestion);
-        answer = (ListView)findViewById(R.id.answer);
+        answer_list = (ListView)findViewById(R.id.answer);
         InputStream is;
         String URL1 = "http://sharp-snow-6521.herokuapp.com/countries.json";
         JSONObject jsonObj;
@@ -56,7 +61,19 @@ public class Question extends Activity implements OnClickListener {
 			JSONArray questions = jsonObj.getJSONArray("questions");
 			String question = questions.getJSONObject(0).getString("question");
 			countryQuestion.setText(question);
-	        
+			
+			JSONArray answers = jsonObj.getJSONArray("answers");
+			HashMap answ = new HashMap();
+			for(int i = 0; i < answers.length(); i++)
+			{
+				String ans = answers.getJSONObject(i).getString("answer");
+				boolean good = answers.getJSONObject(i).getBoolean("good");
+				answ.put(ans, good);
+			}
+	        List<String> answerList = new ArrayList<String>(answ.keySet());
+	        String[] Answers = answerList.toArray(new String[answerList.size()]);
+			setListAdapter(new ArrayAdapter<String>(this, R.id.answer, Answers));
+		
 		} catch (MalformedURLException e) {
 			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
 			alertDialog.setTitle("Error");
